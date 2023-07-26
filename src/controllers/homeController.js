5cd1b2dc215723e67586f229c24281ead240b092
Le Trung Dung <ekkophantoms123@gmail.com>
2023-07-26 09:52:15 +0700

    Update User

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const connection = require('../config/database')
+const { getAllUsers, getUsersbyID, updateUsersbyID } = require('../services/CRUDService')
 
-const getHomepage = (req, res) => {
-    return res.render('homepage.ejs')
+const getHomepage = async (req, res) => {
+    let results = await getAllUsers()
+    return res.render('homepage.ejs', { listUsers: results })
 }
 
 const getLife = (req, res) => {
@@ -12,25 +14,47 @@ const getMy = (req, res) => {
     res.render('sample.ejs')
 }
 
-const postNewUser = (req, res) => {
+const postNewUser = async (req, res) => {
     let email = req.body.email
     let name = req.body.name
     let city = req.body.city
 
     console.log("infor: ", email, name, city)
 
-    connection.query(
-        `INSERT INTO 
-        Users (email, name, city)
-        VALUES (?, ?, ?)`,
-        [email, name, city],
-        function (err, results) {
-            console.log(results)
-            res.send('Create User Success')
-        }
+    let [results, fields] = await connection.query(
+        `INSERT INTO  Users (email, name, city) VALUES (?, ?, ?)`, [email, name, city],
     )
+
+
+    res.send('Created user successfully!!!')
+}
+
+const getCreatePage = (req, res) => {
+    res.render('create.ejs')
+}
+
+const getUpdatePage = async (req, res) => {
+    const userId = req.params.id
+    let user = await getUsersbyID(userId)
+    res.render('edit.ejs', { userEdit: user })
+}
+
+const postUpdateUser = async (req, res) => {
+    let email = req.body.email
+    let name = req.body.name
+    let city = req.body.city
+    let userId = req.body.userId
+
+    console.log("infor: ", email, name, city, userId)
+
+    await updateUsersbyID(email, name, city, userId)
+
+    // res.send('Updated user successfully!!!')
+    //Comeback home directly
+    res.redirect('/')
 }
 
 module.exports = {
-    getHomepage, getLife, getMy, postNewUser
+    getHomepage, getLife, getMy, postNewUser, getCreatePage
+    , getUpdatePage, postUpdateUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getHomepage, getLife, getMy, postNewUser } = require('../controllers/homeController')
+const { getHomepage, getLife, getMy, postNewUser, getCreatePage, getUpdatePage, postUpdateUser } = require('../controllers/homeController')
 
 //router.method('/route', handler)
 router.get('/', getHomepage)
 router.get('/life', getLife)
 router.get('/my', getMy)
+router.get('/create', getCreatePage)
+router.get('/update/:id', getUpdatePage)
 router.post('/create-user', postNewUser)
+router.post('/update-user', postUpdateUser)
+
 
 module.exports = router
